ffc188dd10937824a34b49d579fb48ac8b6e9adf
FelipeDuan <felipeduan1@gmail.com>
2025-08-09 21:05:13 -0300

    feat: add cart item

---
+++ src/components/core/cart-item.tsx
@@ -0,0 +1,63 @@
+import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
+import Image from "next/image";
+import { formatCentsToBRL } from "@/helpers/money";
+import { Button } from "../ui/button";
+
+interface CartItemProps {
+  id: string;
+  productName: string;
+  productVariantName: string;
+  productVariantImageUrl: string;
+  productVariantPriceInCents: number;
+  quantity: number;
+}
+
+export function CartItem({
+  id,
+  quantity,
+  productName,
+  productVariantName,
+  productVariantImageUrl,
+  productVariantPriceInCents,
+}: CartItemProps) {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <Image
+          src={productVariantImageUrl}
+          alt={productVariantName}
+          width={78}
+          height={78}
+          className="rounded-lg"
+        />
+
+        <div className="flex flex-col gap-1">
+          <p className="text-sm font-semibold">{productName}</p>
+          <p className="text-xs font-medium text-muted-foreground">
+            {productVariantName}
+          </p>
+          <div className="flex items-center w-[100px] p-1 border justify-between rounded-lg">
+            <Button className="w-4 h-4" variant={"ghost"} onClick={() => {}}>
+              <MinusIcon className="size-3" />
+            </Button>
+
+            <p className="text-xs">{quantity}</p>
+
+            <Button className="w-4 h-4" variant={"ghost"} onClick={() => {}}>
+              <PlusIcon className="size-3" />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-end gap-2">
+        <Button variant={"outline"} size={"icon"}>
+          <TrashIcon />
+        </Button>
+        <p className="text-sm font-bold">
+          {formatCentsToBRL(productVariantPriceInCents)}
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/core/cart.tsx
+++ src/components/core/cart.tsx
@@ -2,7 +2,6 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { ShoppingBasketIcon } from "lucide-react";
-import Image from "next/image";
 import { getCart } from "@/actions/get-cart";
 import { Button } from "@/components/ui/button";
 import {
@@ -12,6 +11,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { CartItem } from "./cart-item";
 
 export function Cart() {
   const { data: cart, isPending: cartIsLoading } = useQuery({
@@ -30,15 +30,17 @@ export function Cart() {
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-        <div>
+        <div className="space-y-6 px-5">
           {cartIsLoading && <div>Carregando...</div>}
           {cart?.items.map((item) => (
-            <Image
+            <CartItem
               key={item.id}
-              alt={item.productVariant.product.name}
-              src={item.productVariant.imageUrl}
-              width={100}
-              height={100}
+              id={item.id}
+              productName={item.productVariant.product.name}
+              productVariantName={item.productVariant.name}
+              productVariantImageUrl={item.productVariant.imageUrl}
+              productVariantPriceInCents={item.productVariant.priceInCents}
+              quantity={item.quantity}
             />
           ))}
         </div>
